e2e7af3fd20077fba80e40ecc5c7deaf73e62a79
Nwogu Michael <mikedbchi@gmail.com>
2022-12-03 05:39:02 +0100

    update

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex justify-center py-10">
-       <img src="src/assets/images/logo.svg" alt="logo">
+       <img src="../assets/images/logo.svg" alt="logo">
     </div>
 </template>
 <script>
